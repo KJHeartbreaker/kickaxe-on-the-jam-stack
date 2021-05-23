7c5e96b3139b7cfabc25f866dce88532ebb69392
KJ-tasktop <kj.obrien@tasktop.com>
2021-05-22 18:23:06 -0700

    commented log to deploy

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -55,7 +55,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         <p><strong>Message: </strong>${formData.message}</p><br>`
         })
 
-        console.log('Message Sent', email.messageId)
+        // console.log('Message Sent', email.messageId)
     } catch (error) {
         console.log('error:', error)
     }
